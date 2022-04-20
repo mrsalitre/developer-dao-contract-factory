9c5e28c79065c7d55ee2b145ef6e9ad8dafc982d
Jean Carlos A. Ortiz <ayalajeancarlos.91@gmail.com>
2022-04-20 21:10:58 +0100

    chore: basic navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,110 @@
+<template>
+  <nav class="fixed w-full z-30 top-0 bg-white shadow">
+        <div
+          class="max-w-screen-xl mx-auto py-2 px-4 sm:px-6 lg:px-8 lg:flex lg:items-center lg:justify-between"
+        >
+          <div class="flex items-center justify-between">
+            <nuxt-link
+              class="no-underline hover:no-underline text-2xl font-bold text-iberinsect flex items-center flex-wrap"
+              to="/"
+            >
+              <img
+                class="w-12 h-auto inline-block"
+                src="/icon.png"
+              />
+            </nuxt-link>
+            <div class="block lg:hidden">
+              <button
+                class="flex items-center px-3 py-2 border rounded"
+                aria-label="show menu button"
+                @click="showMenu = !showMenu"
+              >
+                <svg
+                  class="fill-current h-3 w-3"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div
+            class="w-full block flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block"
+          >
+            <div
+              class="list-reset lg:flex justify-end flex-1 items-center mb-4 lg:mb-0 pl-4 text-iberinsect-dark"
+              @click.prevent="showMenu = false"
+            >
+              <div v-for="menuItem in menuItems" :key="menuItem.route">
+                <nuxt-link
+                  :to="menuItem.route"
+                  class="block py-2 lg:px-4 no-underline hover:text-underline"
+                  :class="{
+                    'font-bold': Object.is(
+                      currentRoute,
+                      menuItem.name === 'Home'
+                        ? 'index'
+                        : menuItem.name
+                    ),
+                  }"
+                >
+                  {{ menuItem.name }}
+                </nuxt-link>
+              </div>
+            </div>
+          </div>
+          <div
+            v-show="showMenu"
+            class="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:hidden text-iberinsect-dark"
+          >
+            <div
+              class="list-reset lg:flex justify-end flex-1 items-center mb-4 lg:mb-0 py-8 pb-12 max-h-screen overflow-y-scroll"
+              @click.prevent="showMenu = false"
+            >
+              <div v-for="menuItem in menuItems" :key="`mobile-${menuItem.route}`">
+                <nuxt-link
+                  :to="menuItem.route"
+                  class="block py-2 lg:px-4 no-underline hover:text-underline"
+                  :class="{
+                    'font-bold': Object.is(
+                      currentRoute,
+                      menuItem.name === 'Home'
+                        ? 'index'
+                        : menuItem.name
+                    ),
+                  }"
+                >
+                  {{ menuItem.name }}
+                </nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+</template>
+<script>
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      showMenu: false,
+      menuItems: [
+        {
+          name: 'Home',
+          route: '/',
+        },
+        {
+          name: 'Create Contract',
+          route: '/create-contract',
+        }
+      ],
+    }
+  },
+  computed: {
+    currentRoute() {
+      return this.$nuxt.$route.params.uid || this.$nuxt.$route.name
+    },
+  },
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NavBar />
+    <nuxt />
+  </div>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
