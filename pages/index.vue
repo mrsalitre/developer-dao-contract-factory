387096f62453b26c160e11c6c60a64583efcc2a4
Jean Carlos A. Ortiz <ayalajeancarlos.91@gmail.com>
2022-05-09 16:51:17 +0100

    style: generated contract list

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <section v-if="!user" class="flex h-screen">
       <div class="m-auto text-gray-500">
-        <h1 class="text-7xl md:text-9xl">
+        <h1 class="text-7xl font-extralight md:text-9xl">
           Gain true ownership of your NFT collection by owning your own smart
           contracts*
         </h1>
@@ -29,22 +29,33 @@
     </section>
     <section v-else-if="user && generatedContracts" class="pt-20">
       <div>
-        <h2>Select a contract</h2>
+        <h2 class="text-2xl text-gray-500 font-light">Select a contract</h2>
         <ul class="md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-8 md:gap-10">
           <li
             v-for="(contract, index) in generatedContracts"
             :key="index"
-            class="mt-4 shadow z-20"
+            class="mt-4 shadow bg-white rounded-lg"
           >
             <div
-              class="flex rounded-lg flex-col justify-between h-full bg-white"
+              class="flex flex-col justify-between h-full  px-2 py-3"
             >
-              <div>
-                <span>{{ contract[0] }}</span>
+              <div class="pb-3">
+                <p class="text-xl font-bold text-gray-500 overflow-scroll">{{ contract[0] }}</p>
               </div>
-              <div>
-                <p>botones</p>
-              </div>
+              <div class="flex justify-between">
+                    <nuxt-link
+                      :to="`/edit-contract/${contract[2]}`"
+                      class="bg-white border py-2 px-2 w-auto rounded shadow-md hover:shadow-none"
+                    >
+                      Edit
+                    </nuxt-link>
+                    <nuxt-link
+                      :to="`/create-nft/${contract[2]}`"
+                      class="bg-white border py-2 px-2 w-auto rounded shadow-md hover:shadow-none"
+                    >
+                      Create NFT
+                    </nuxt-link>
+                  </div>
             </div>
           </li>
         </ul>
