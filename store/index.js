a570c8ce9977b29c7926b78beae4db84ee42aeaf
Jean Carlos A. Ortiz <ayalajeancarlos.91@gmail.com>
2022-06-20 05:36:38 +0100

    feat: gas-less meta transaction - also a fallbak in case the relayer is without funds

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 // export the state
 export const state = () => ({
-  factoryAddress: '0x6401A059F9cD1EE1afb82f49b4d3db4d679e7f1f',
+  factoryAddress: '0xbdd68948D97D523623Bb15BC3747953671311B98',
+  forwarderAddress: '0x3424E811eaeeD59CE5Fb7A09EA3473f8c27896eE'
 })
